f8f62293d58b6fe3a0e181bae60f604efadb2cf4
MaxRybetsky <stsdewar@ya.ru>
2021-04-29 11:27:16 +0300

    Update exceptions with constructors.

--- src/main/java/com/example/birdsapp/exceptions/NoSuchElementException.java
+++ src/main/java/com/example/birdsapp/exceptions/NoSuchElementException.java
@@ -1,4 +1,10 @@
 package com.example.birdsapp.exceptions;
 
 public class NoSuchElementException extends Exception {
+    public NoSuchElementException() {
+    }
+
+    public NoSuchElementException(String message) {
+        super(message);
+    }
 }
