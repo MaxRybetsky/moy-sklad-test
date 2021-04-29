f4aec73b7434d83b5e3dd5e96192d351ca9d796e
MaxRybetsky <stsdewar@ya.ru>
2021-04-29 10:40:01 +0300

    Add Services.

---
+++ src/main/java/com/example/birdsapp/service/api/BaseCrudService.java
@@ -0,0 +1,47 @@
+package com.example.birdsapp.service.api;
+
+import com.example.birdsapp.exceptions.ElementAlreadyExistsException;
+import com.example.birdsapp.exceptions.NoSuchElementException;
+
+/**
+ * Base CRUD Service.
+ *
+ * @param <T> Custom DTO
+ */
+public interface BaseCrudService<T> {
+    /**
+     * Adds element to storage.
+     *
+     * @param elem Element to add.
+     * @return Element with updated ID parameters.
+     * @throws ElementAlreadyExistsException If such element already exists.
+     */
+    T create(T elem) throws ElementAlreadyExistsException;
+
+    /**
+     * Reads element from storage by its ID.
+     *
+     * @param id Id of element.
+     * @return Element with specifying ID if presents.
+     * @throws NoSuchElementException If there is no such element in storage.
+     */
+    T readById(Long id) throws NoSuchElementException;
+
+    /**
+     * Updates element with new data.
+     *
+     * @param elem Element with new data.
+     * @return Updated element.
+     * @throws NoSuchElementException If there is no such element in storage.
+     */
+    T update(T elem) throws NoSuchElementException;
+
+    /**
+     * Deletes element from storage by its ID.
+     *
+     * @param id ID of element to delete.
+     * @return Deleted element.
+     * @throws NoSuchElementException If there is no such element in storage.
+     */
+    T deleteById(Long id) throws NoSuchElementException;
+}
