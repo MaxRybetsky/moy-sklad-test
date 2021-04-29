d91bd16d1fd772638c4176363e386f980fb1ac1e
Рыбецкий Максим Александрович <mrybetskiy@phoenixit.ru>
2021-04-29 09:14:11 +0300

    Initial commit

---
+++ src/test/java/com/example/birdsapp/BirdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.birdsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
